1f7bde41b69d4e9b363c42602b9d396ec5219f91
SouzaNicholas <76216808+SouzaNicholas@users.noreply.github.com>
2022-02-19 15:39:28 -0500

    Pushing an unstable version to be reviewed. Previously stable functionality remains untouched.

--- DB.py
+++ DB.py
@@ -24,12 +24,17 @@ def init_logs_table(curs: sql.Cursor):
     );""")
 
 
+# The idea here is to use LIKE to find any instance of select terms entered by the user
+# The commented out block was my original idea that is giving undocumented errors.
+# the block after is my attempt to fix it, it is not currently coherent.
 def query_db(terms: dict):
     db = open_db()
-    print(terms.values())
-    # db[1].execute("""SELECT date, emotion, action, cause FROM logs WHERE
+    # db[1].execute("""SELECT log_date, emotion, log_action, cause FROM logs WHERE
     #                     log_date LIKE (?) AND
     #                     emotion LIKE (?) AND
     #                     log_action LIKE (?) AND
     #                     cause LIKE (?)""", (terms.values()))
+    r = db[1].execute("""SELECT log_date, emotion, log_action, cause FROM logs WHERE
+                        log_date LIKE ('%' || ? || '%');""", "A")
+    print(r.fetchall())
     close_db(db[0])
